38c8c99522815aede6cf23e36b9e1ebf7c227fde
uraankhayayaal <uraankhayayaal@gmail.com>
2019-08-03 15:58:49 +0900

    updates

--- js/init.js
+++ js/init.js
@@ -2,7 +2,7 @@ $('.ml6 .letters').each(function(){
     $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
 });
   
-anime.timeline({loop: true})
+anime.timeline({loop: false})
     .add({
       targets: '.ml6 .letter',
       translateY: ["1.1em", 0],
@@ -11,30 +11,19 @@ anime.timeline({loop: true})
       delay: function(el, i) {
         return 3000 + 50 * i;
       }
-    }).add({
-      targets: '.ml6',
-      opacity: 0,
-      duration: 1000,
-      easing: "easeOutExpo",
-      delay: 1000
     });
 
-anime.timeline({loop: true})
+anime.timeline({loop: false})
     .add({
         targets: '.logo',
         rotate: '1turn',
         scale: 1,
         duration: 5000,
-    })
-    .add({
-        targets: '.logo',
-        scale: 0,
-        duration: 1000,
     });
-anime.timeline({loop: true})
+anime.timeline({loop: false})
     .add({
         targets: '.logo img',
-        loop: true,
+        loop: false,
         rotate: '1turn',
         duration: 5000,
         endDelay: 1000,
